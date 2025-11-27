9fad2adb6ec6874c2448e0649bd977401b2d0e15
Arun <vsuarun@gmail.com>
2025-11-27 12:16:46 +0530

    update workflow

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "9wjah3",
+    projectId: "mdt7jm",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // You can return config if you've modified it
